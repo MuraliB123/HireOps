f62275d9d934f01a043e102955affa7aafb4d36d
MuraliB123 <muralibabu1729@gmail.com>
2024-01-24 09:11:46 +0530

    forecast

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ import pandas as pd
 from flask_cors import CORS
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
+import matplotlib.pyplot as plt
+import numpy as np
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.arima.model import ARIMA
 app = Flask(__name__, static_folder='static')
 CORS(app)
 app.secret_key = 'mykey'
@@ -84,6 +88,57 @@ def file_json():
     else:
         return render_template("index.html")
 
+@app.route('/forecast',methods=['GET','POST'])
+def get_input():
+    if request.method == 'POST':
+        upload_file = request.files['jsonfile']
+        if upload_file and upload_file.filename.endswith('.json'):
+            json_data = upload_file.read()
+            data = json.loads(json_data)
+            data_0 = data.get('EmployeeTurnoverRate')
+            data_1 = data.get('EmployeeRetentionRate')
+            data_2 = data.get('OrganisationExpansionIndex')
+            data_3 = data.get('NumberOfLargeScaleProjects')
+            data_4 = data.get('NumberOfMediumAndSmallScaleProjects')
+            data_7 = data.get('NIFTYIT')
+            data_8 = data.get('GDP')
+            data_9 = data.get('GrossProfitMargin')
+            data_10 = data.get('WorkingCapital')
+            data_11 = data.get('QuickRatio')
+            data_12 = data.get('ReturnOnEquity')
+            data_to_predict = [[data_0, data_1, data_2, data_3, data_4,data_7, data_8, data_9, data_10, data_11, data_12]]
+            dataset = pd.read_csv("/home/murali/Documents/HireOps/forecast_hiring - Sheet1(1).csv")
+            Y = dataset['Candidates Hired']
+            X = dataset.drop("Candidates Hired",axis=1)
+            #Model1
+            from sklearn.ensemble import RandomForestRegressor
+            model = RandomForestRegressor()
+            model.fit(X,Y)
+            predicted_candidates_hired_1 = model.predict(data_to_predict)
+            #Model2
+            from sklearn.neighbors import KNeighborsRegressor
+            knn_model = KNeighborsRegressor(n_neighbors=3)  
+            knn_model.fit(X,Y)
+            predicted_candidates_hired_2 = knn_model.predict(data_to_predict)
+            #time series analysis
+            df = pd.DataFrame({'Value':Y})
+            df['Value_diff'] = df['Value'].diff()
+            df['Value_diff_abs'] = df['Value_diff'].abs()
+            df = df.dropna()
+            model = ARIMA(Y, order=(1, 0, 0))
+            model = model.fit()
+            forecast_steps = 4  
+            forecast_results = model.get_forecast(steps=forecast_steps)
+            forecast_values = forecast_results.predicted_mean
+            plt.plot(Y, label='Original Time Series', color='blue')
+            plt.plot(range(len(Y), len(Y) + forecast_steps), forecast_values, label='Forecasted Values', color='red')
+            plt.xlabel('Time')
+            plt.ylabel('Value')
+            plt.title('ARIMA Forecasting')
+            plt.legend()
+            plt.savefig('static/forecast_plot.png')  
+            plt.close()
+            return render_template("final_result.html",prediction=(predicted_candidates_hired_1+predicted_candidates_hired_2)/2)
 
 
 if __name__ == '__main__':
